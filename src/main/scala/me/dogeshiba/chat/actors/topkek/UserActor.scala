88b3b612671e24736dbb5eee2f26ee5afacb80d0
dogeshibu <cthulhu218@yandex.ru>
2015-06-20 18:10:39 +0300

    Fix build

--- src/main/scala/me/dogeshiba/chat/actors/topkek/UserActor.scala
+++ src/main/scala/me/dogeshiba/chat/actors/topkek/UserActor.scala
@@ -1,11 +1,16 @@
 package me.dogeshiba.chat.actors.topkek
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{Actor, ActorRef, PoisonPill, _}
 import akka.pattern._
 import me.dogeshiba.chat.actors.topkek.Messages.UserDisconnected
 import me.dogeshiba.chat.protocols.topkek.Messages._
 
 class UserActor(private[this] val broadcaster: ActorRef) extends Actor {
+
+  implicit val timeout = akka.util.Timeout(5, TimeUnit.SECONDS)
+  implicit val executionContext = context.dispatcher
 
   private[this] def userLogined: Receive = {
     case msg : Request =>
